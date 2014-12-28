218c55220e53f3b1d165e7920a6fd0b9e4a4fa69
zhanglei13 <zhanglei13@otcaix.iscas.ac.cn>
2014-12-28 12:39:49 +0800

    solutions from zhanglei

--- WeeklyCode/week1/Two Sum/zhanglei.java
+++ WeeklyCode/week1/Two Sum/zhanglei.java
@@ -1,12 +1,13 @@
 public class Solution {
 	public int[] twoSum(int[] numbers, int target) {
 		Map<Integer, Integer> index = new HashMap<Integer, Integer>();
-		int[] result = new int[2];
+		int[] result = { -1, -1 };
 
 		for (int i = 0; i < numbers.length; i++) {
 			if (index.containsKey(numbers[i])) {
 				result[0] = index.get(numbers[i]);
 				result[1] = i + 1;
+				return result;
 			} else {
 				index.put(target - numbers[i], i + 1);
 			}
