51f10eb59807afaa88c56c95443996e4fd68a149
rkasdf <rkasdfg@gmail.com>
2014-12-29 20:00:53 +0800

    remove bug

--- WeeklyCode/week2/Remove Duplicates from Sorted Array II/renkai.py
+++ WeeklyCode/week2/Remove Duplicates from Sorted Array II/renkai.py
@@ -4,7 +4,6 @@ class Solution:
         index = 0
         for i in range(len(A)):
             if i > 0 and i < len(A)-1 and A[i] == A[i-1] and A[i] == A[i+1]:
-                print i
                 continue
             A[index] = A[i]
             index += 1
