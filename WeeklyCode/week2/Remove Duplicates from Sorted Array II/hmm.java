2d95a23091a72f73c7694e440cbe03ea838cddbc
hemeimei <315627330@qq.com>
2015-01-04 16:57:29 +0800

    update last week

--- WeeklyCode/week2/Remove Duplicates from Sorted Array II/hmm.java
+++ WeeklyCode/week2/Remove Duplicates from Sorted Array II/hmm.java
@@ -1,21 +1,11 @@
 public class Solution {
     public int removeDuplicates(int[] A) {
-        if(A == null || A.length <1) return 0;
-        int length = A.length;
-        int currNum = A[0], currCount = 1, currOffset=0;
-        for(int i=1;i<length;i++){
-            if(A[i] == currNum){
-                if(currCount == 2){
-                    currOffset++;
-                }else{
-                    currCount++;
-                }
-            }else{
-                currNum = A[i];
-                currCount=1;
+        int index = 0;
+        for(int i=0;i<A.length;i++){
+            if(i >= A.length-2 || A[i]!=A[i+2]){
+                A[index++] = A[i];
             }
-            A[i-currOffset] = A[i];
         }
-        return length-currOffset;
+        return index;
     }
 }
