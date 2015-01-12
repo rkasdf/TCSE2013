0f9ee871bfa285d1484d3a20c71421d143f4a2f4
daisyonly <1241945585@qq.com>
2015-01-12 16:02:01 +0800

    czh

--- WeeklyCode/week4/Linked List Cycle II/czh.java
+++ WeeklyCode/week4/Linked List Cycle II/czh.java
@@ -1,7 +1,3 @@
-package week4;
-
-import week3.ListNode;
-
 public class LinkedListCycleII {
 	
 	private ListNode move(ListNode node,int count) {
@@ -45,8 +41,51 @@ public class LinkedListCycleII {
 		return curNode;    
     }
 	
+	//O(nlogn) 找到相遇点后二分 头结点到相遇点之间的节点，直到找到环的入口点
+	public  ListNode detectCycle2(ListNode head) {
+		if(head == null) return null;
+		ListNode single = head;
+		ListNode node = null;
+		ListNode dual = head;
+		int sum=0;
+		while(dual!=null && dual.next != null){
+			sum++;
+			single = single.next;
+			dual = dual.next.next;
+			if(single == dual){
+				node = single;
+				break;
+			}
+		}
+		
+		if(dual == null || dual.next == null){
+			 return null;
+		}
+		
+		ListNode curNode = node.next;
+		int count = 1;
+		while(curNode != node){
+			count++;
+			curNode=curNode.next;
+		}
+		
+		int l = 0, r = sum;
+		ListNode head2= head;
+		while(l<r){
+			int mid = (l+r)/2;
+			curNode = move(head2, mid-l);
+			if(curNode != move(curNode, count)){
+				l = mid + 1;
+				head2 = curNode.next;
+			}else{
+				r = mid;
+			}
+		}
+		return head2;    
+    }
+	
 	//O(n) 参考题解
-	public static ListNode detectCycle2(ListNode head) {
+	public ListNode detectCycle3(ListNode head) {
 		if(head == null) return null;
 		ListNode single = head;
 		ListNode dual = head;		
