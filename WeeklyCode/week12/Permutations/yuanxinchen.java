8cff1b10e35efc573c09fd9c831de8d1410ad84a
UcasRichard <lccpowter@gmail.com>
2015-04-13 20:47:12 +0800

    o(╯□╰)o，我就是闲得无聊

--- WeeklyCode/week12/Permutations/yuanxinchen.java
+++ WeeklyCode/week12/Permutations/yuanxinchen.java
@@ -1,29 +1,30 @@
 public class Permutations {
-    public List<List<Integer>> permute(int[] num) {
-        List<List<Integer>> res = new ArrayList<>();
-        permute(res, new LinkedList<>(), num, 0);
-        return res;
-    }
+	public List<List<Integer>> permute(int[] num) {
+		List<List<Integer>> res = new ArrayList<>();
+		permute(res, new LinkedList<>(), num, 0);
+		return res;
+	}
 
-    private void permute(List<List<Integer>> res, List<Integer> item, int[] num, int begin) {
-    	if(item.size() == num.length) {
-    		res.add(new ArrayList<>(item));
-    		return;
-    	}
-    	for(int i = begin; i < num.length; i++) {
-    		swap(num, begin, i);
-    		item.add(num[begin]);
-    		permute(res, item, num, begin + 1);
-    		item.remove(item.size() - 1);
-    		swap(num, begin, i);
-    	}
-    }
+	private void permute(List<List<Integer>> res, List<Integer> item,
+			int[] num, int begin) {
+		if (item.size() == num.length) {
+			res.add(new ArrayList<>(item));
+			return;
+		}
+		for (int i = begin; i < num.length; i++) {
+			swap(num, begin, i);
+			item.add(num[begin]);
+			permute(res, item, num, begin + 1);
+			item.remove(item.size() - 1);
+			swap(num, begin, i);
+		}
+	}
 
-    private void swap(int[] num, int left, int right) {
-    	int tmp = num[left];
-    	num[left] = num[right];
-    	num[right] = tmp;
-    }
+	private void swap(int[] num, int left, int right) {
+		int tmp = num[left];
+		num[left] = num[right];
+		num[right] = tmp;
+	}
 }
 
 public class PermutationsII {
@@ -41,8 +42,9 @@ public class PermutationsII {
 			return;
 		}
 		for (int i = 0; i < num.length; i++) {
-			if(i != 0 && !visited[i - 1] && num[i] == num[i - 1]) continue;
-			if(!visited[i]) {
+			if (i != 0 && !visited[i - 1] && num[i] == num[i - 1])
+				continue;
+			if (!visited[i]) {
 				visited[i] = true;
 				item.add(num[i]);
 				permuteUnique(res, item, num, visited);
